1c002935689988a49d3d109d135b32e5f271e99c
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-06-13 11:52:26 +0300

    add store and bookSlice

---
+++ src/redux/books/booksSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  books: [],
+};
+
+const bookSlice = createSlice({
+  name: 'books',
+  initialState,
+});
+
+console.log(bookSlice);
+
+export default bookSlice.reducer;
